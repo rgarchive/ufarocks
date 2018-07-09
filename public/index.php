e06f03c5e8a5025dc279ec463e0ea31c1c472e50
Rustem Gareev <git@rustemgareev.com>
2018-07-09 12:44:40 +0500

    feat: add global variable `is_localhost`

--- public/index.php
+++ public/index.php
@@ -9,45 +9,15 @@
 
 define('LARAVEL_START', microtime(true));
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| our application. We just need to utilize it! We'll simply require it
-| into the script here so that we don't have to worry about manual
-| loading any of our classes later on. It feels great to relax.
-|
-*/
+// Check if we are on localhost
+$GLOBALS['is_localhost'] = in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')) ? true : false;
 
-require __DIR__.'/../vendor/autoload.php';
+// Get the relative path to the application
+$app_directory = $GLOBALS['is_localhost'] ? '' : '/laravel-ufarocks';
 
-/*
-|--------------------------------------------------------------------------
-| Turn On The Lights
-|--------------------------------------------------------------------------
-|
-| We need to illuminate PHP development, so let us turn on the lights.
-| This bootstraps the framework and gets it ready for use, then it
-| will load up this application so that we can run it and send
-| the responses back to the browser and delight our users.
-|
-*/
+require __DIR__.'/..'.$app_directory.'/vendor/autoload.php';
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
-*/
+$app = require_once __DIR__.'/..'.$app_directory.'/bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
